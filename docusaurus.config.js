fdda39367812a184ba8621210b15895ecba99752
shu <3138066125@qq.com>
2023-07-22 15:46:09 +0800

    跟新

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,11 +8,11 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "拿铁要加冰 | Java开发工程师", // 标题 | Title
   tagline: "活到老，学到老，作为程序员的自我修养，哈哈哈哈哈", // 标语 | Tagline
-  url: "https://www.shuzhilin.top", // 站点地址 | Your website URL
+  url: "https://www.lottecoffee.com", // 站点地址 | Your website URL
   baseUrl: "/", // 站点根目录 | Base URL for your project */
   favicon: "img/favicon.ico", // 站点图标 | Favicon
-  organizationName: "shuzhilin", // 拥有这个仓库的 GitHub 用户或组织|Usually your GitHub org/user name.
-  projectName: "shuzhilin.top", // 仓库名称 | Usually your repo name.
+  organizationName: "lottecoffee", // 拥有这个仓库的 GitHub 用户或组织|Usually your GitHub org/user name.
+  projectName: "www.lottecoffee.com", // 仓库名称 | Usually your repo name.
   i18n: {
     defaultLocale: "zh-Hans",
     locales: ["zh-Hans"],
@@ -24,23 +24,21 @@ const config = {
       ({
         docs: {
           sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://git.7wate.com/zhouzhongping/wiki/src/branch/master",
           showLastUpdateAuthor: true,
           showLastUpdateTime: true,
           breadcrumbs: false,
         },
         blog: {
-          blogTitle: "7Wate`s Blog",
-          blogDescription: "7Wate 的个人生活和工作记录",
+          blogTitle: "",
+          blogDescription: "拿铁要加冰 的个人生活和工作记录",
           blogSidebarCount: 7,
           blogSidebarTitle: "近期文章",
           showReadingTime: true,
-          editUrl: "https://git.7wate.com/zhouzhongping/wiki/src/branch/master",
           feedOptions: {
-            title: "7Wate`s Blog",
-            description: "7Wate 的个人生活和工作记录",
+            title: "",
+            description: "拿铁要加冰 的个人生活和工作记录",
             type: "all",
-            copyright: `Copyright © ${new Date().getFullYear()} 7Wate, Inc.`,
+            copyright: `Copyright © ${new Date().getFullYear()} 拿铁要加冰, Inc.`,
           },
         },
         theme: {
@@ -238,7 +236,7 @@ const config = {
       },
       umami: {
         websiteid: "7efcd733-c232-43db-9f17-10a00c53b152",
-        src: "https://www.shuzhilin.top/umami.js",
+        src: "https://www.lottecoffee.com/umami.js",
       },
       footer: {
         style: "dark",
