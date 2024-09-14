f2ef5cca6ca201b355f62c3a1bf5cb91fa61ef18
Eason-shu <3138066125@qq.com>
2024-09-14 22:01:55 +0800

    修改样式

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,8 +6,8 @@
  * @FilePath: \CoffeeLatte\docusaurus.config.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
-const lightCodeTheme = require("prism-react-renderer/themes/nightOwlLight");
-const darkCodeTheme = require("prism-react-renderer/themes/nightOwl");
+const lightCodeTheme = require("prism-react-renderer/themes/github");
+const darkCodeTheme = require("prism-react-renderer/themes/vsDark");
 const { to } = require("react-spring");
 /** @type {import('@docusaurus/types').Config} */
 const config = {
@@ -465,7 +465,18 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
         defaultLanguage: "markdown",
-        additionalLanguages: ["java", "git", "nginx", "http"]
+        additionalLanguages: ["java", "git", "nginx", "http", "python", "css", "javascript", "bash", "sql", "cpp", "go", "typescript", "yaml", "scss"],
+        magicComments: [
+          {
+            className: 'theme-code-block-highlighted-line',
+            line: 'highlight-next-line',
+            block: {start: 'highlight-start', end: 'highlight-end'},
+          },
+          {
+            className: 'code-block-error-line',
+            line: 'This will error',
+          },
+        ],
       },
       mermaid: {
         theme: { light: "neutral", dark: "forest" },
